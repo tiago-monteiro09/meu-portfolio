1825403dc745e84dc7a1677fd23d3e12e2b3207c
tiago-monteiro09 <2009.tiagomonteiro@gmail.com>
2026-02-04 15:22:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('🚀 Portfolio carregado com sucesso!');
+
+// Teste: mudar cor de fundo ao clicar
+document.body.addEventListener('click', () => {
+    console.log('Clicaste na página!');
+});
